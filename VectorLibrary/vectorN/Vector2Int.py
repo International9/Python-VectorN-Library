5c766d9eba7c8978d933cf9d91100acf1893e2bf
International9 <nirben69@gmail.com>
2025-02-02 16:48:45 +0200

    Update Vector2Int.py

--- VectorLibrary/vectorN/Vector2Int.py
+++ VectorLibrary/vectorN/Vector2Int.py
@@ -1,8 +1,8 @@
 from math import acos
 
-# Vector2 Class
-class Vector2:
-    def __init__(self, x : float, y : float):
+# Vector2Int Class
+class Vector2Int:
+    def __init__(self, x : int, y : int):
         self.x = x
         self.y = y
 
@@ -10,7 +10,7 @@ class Vector2:
 
     # Setters:
     @property
-    def x(self):
+    def x(self) -> int:
         return self._x
 
     @x.setter
@@ -18,10 +18,10 @@ class Vector2:
         if (not isinstance(value, (int, float)) or isinstance(value, bool)):
             raise TypeError("Cannot Set X To That Type! Only Int Or Float.")
         
-        self._x = value
+        self._x = int(value)
 
     @property
-    def y(self):
+    def y(self) -> int:
         return self._y
 
     @y.setter
@@ -29,147 +29,153 @@ class Vector2:
         if (not isinstance(value, (int, float)) or isinstance(value, bool)):
             raise TypeError("Cannot Set Y To That Type! Only Int Or Float.")
         
-        self._y = value
-
+        self._y = int(value)
 
 
 
     @property
-    def magnitude(self):
+    def magnitude(self) -> float:
         return (self.x * self.x + self.y * self.y) ** 0.5
 
     @property
     def Normalized(self):
-        return self / self.magnitude
-
+        mag = self.magnitude
+        return Vector2Int(self.x / mag, self.y / mag)
+    
+    
     # Regular Instance Methods:
     def Normalize(self):
-        self /= self.magnitude
+        mag = self.magnitude
+        self.x /= mag
+        self.y /= mag
+
+
+
 
     # Static Methods
     @staticmethod
-    def Dot(a : "Vector2", b : "Vector2") -> float:
-        if not isinstance(a, Vector2) or not isinstance(b, Vector2):
-            raise TypeError("Both Arguments Must Be Of Type Vector2.")
+    def Dot(a : "Vector2Int", b : "Vector2Int") -> float:
+        if not isinstance(a, Vector2Int) or not isinstance(b, Vector2Int):
+            raise TypeError("Both Arguments Must Be Of Type Vector2Int.")
 
         return a.x * b.x + a.y * b.y
 
     @staticmethod
-    def Distance(a : "Vector2", b : "Vector2") -> float:
-        if not isinstance(a, Vector2) or not isinstance(b, Vector2):
-            raise TypeError("Both Arguments Must Be Of Type Vector2.")
+    def Distance(a : "Vector2Int", b : "Vector2Int") -> float:
+        if not isinstance(a, Vector2Int) or not isinstance(b, Vector2Int):
+            raise TypeError("Both Arguments Must Be Of Type Vector2Int.")
 
         return (a - b).magnitude
 
     @staticmethod
-    def Max(a : "Vector2", b : "Vector2"):
-        if not isinstance(a, Vector2) or not isinstance(b, Vector2):
-            raise TypeError("Both Arguments Must Be Of Type Vector2.")
+    def Max(a : "Vector2Int", b : "Vector2Int"):
+        if not isinstance(a, Vector2Int) or not isinstance(b, Vector2Int):
+            raise TypeError("Both Arguments Must Be Of Type Vector2Int.")
 
-        return Vector2(max(a.x, b.x), max(a.y, b.y))
+        return Vector2Int(max(a.x, b.x), max(a.y, b.y))
 
     @staticmethod
-    def Min(a : "Vector2", b : "Vector2"):
-        if not isinstance(a, Vector2) or not isinstance(b, Vector2):
-            raise TypeError("Both Arguments Must Be Of Type Vector2.")
+    def Min(a : "Vector2Int", b : "Vector2Int"):
+        if not isinstance(a, Vector2Int) or not isinstance(b, Vector2Int):
+            raise TypeError("Both Arguments Must Be Of Type Vector2Int.")
 
-        return Vector2(min(a.x, b.x), min(a.y, b.y))
+        return Vector2Int(min(a.x, b.x), min(a.y, b.y))
     
     @staticmethod
-    def Angle(a : "Vector2", b : "Vector2") -> float:
-        if not isinstance(a, Vector2) or not isinstance(b, Vector2):
-            raise TypeError("Both Arguments Must Be Of Type Vector2.")
-            
-        dot_product = Vector2.Dot(a, b)
-        magnitude_product = a.magnitude * b.magnitude
+    def Angle(a : "Vector2Int", b : "Vector2Int") -> float:
+        if not isinstance(a, Vector2Int) or not isinstance(b, Vector2Int):
+            raise TypeError("Both Arguments Must Be Of Type Vector2Int.")
 
+        dot_product = Vector2Int.Dot(a, b)
+        magnitude_product = a.magnitude * b.magnitude
+        
         if magnitude_product == 0: return 0
         
         cos_theta = dot_product / magnitude_product
         return acos(cos_theta)
 
+
     @staticmethod
-    def zero(): return Vector2(0, 0)
+    def zero(): return Vector2Int(0, 0)
     @staticmethod
-    def one(): return Vector2(1, 1)
+    def one(): return Vector2Int(1, 1)
     @staticmethod
-    def right(): return Vector2(1, 0)
+    def right(): return Vector2Int(1, 0)
     @staticmethod
-    def left(): return Vector2(-1, 0)
+    def left(): return Vector2Int(-1, 0)
     @staticmethod
-    def up(): return Vector2(0, 1)
+    def up(): return Vector2Int(0, 1)
     @staticmethod
-    def down(): return Vector2(0, -1)
+    def down(): return Vector2Int(0, -1)
 
 
 
     # Operators
-    def __add__(self, o : "Vector2"):
-        if not isinstance(o, Vector2):
-            raise TypeError("Can Only Add With Type Vector2.")
+    def __add__(self, o : "Vector2Int"):
+        if not isinstance(o, Vector2Int):
+            raise TypeError("Can Only Add With Type Vector2Int.")
 
-        return Vector2(self.x + o.x, self.y + o.y)
+        return Vector2Int(self.x + o.x, self.y + o.y)
 
-    def __sub__(self, o : "Vector2"):
-        if not isinstance(o, Vector2):
-            raise TypeError("Can Only Subtruct With Type Vector2.")
+    def __sub__(self, o : "Vector2Int"):
+        if not isinstance(o, Vector2Int):
+            raise TypeError("Can Only Subtruct With Type Vector2Int.")
 
-        return Vector2(self.x - o.x, self.y - o.y)
+        return Vector2Int(self.x - o.x, self.y - o.y)
 
     def __mul__(self, o : float):
-        return Vector2(self.x * o, self.y * o)
+        return Vector2Int(int(self.x * o), int(self.y * o))
 
     def __truediv__(self, o : float):
-        return Vector2(self.x / o, self.y / o)
-
+        return Vector2Int(self.x // o, self.y // o)
+    
     def __floordiv__(self, o : float):
-        return Vector2(self.x // o, self.y // o)
+        return self / o
 
-    def __iadd__(self, o : "Vector2"):
-        if not isinstance(o, Vector2):
-            raise TypeError("Can Only Add With Type Vector2.")
-
+    def __iadd__(self, o : "Vector2Int"):
+        if not isinstance(o, Vector2Int):
+            raise TypeError("Can Only Add With Type Vector2Int.")
+            
         self.x += o.x
         self.y += o.y
         return self
 
-    def __isub__(self, o : "Vector2"):
-        if not isinstance(o, Vector2):
-            raise TypeError("Can Only Subtruct With Type Vector2.")
+    def __isub__(self, o : "Vector2Int"):
+        if not isinstance(o, Vector2Int):
+            raise TypeError("Can Only Subtruct With Type Vector2Int.")
 
         self.x -= o.x
         self.y -= o.y
         return self
 
     def __imul__(self, o : float):
-        self.x *= o
-        self.y *= o
+        self.x *= int(o)
+        self.y *= int(o)
         return self
 
     def __itruediv__(self, o : float):
-        self.x /= o
-        self.y /= o
-        return self
-    
-    def __ifloordiv__(self, o : float):
         self.x //= o
         self.y //= o
         return self
 
-    def __eq__(self, o : "Vector2"):
-        if not isinstance(o, Vector2):
-            raise TypeError("Can Only Compare With Type Vector2.")
+    def __ifloordiv__(self, o : float):
+        self /= o
+        return self
+
+    def __eq__(self, o : "Vector2Int"):
+        if not isinstance(o, Vector2Int):
+            raise TypeError("Can Only Compare With Type Vector2Int.")
 
         return self.x == o.x and self.y == o.y
+
+    def __ne__(self, o : "Vector2Int"):
+        if not isinstance(o, Vector2Int):
+            raise TypeError("Can Only Compare With Type Vector2Int.")
+
+        return self.x != o.x or self.y != o.y
     
-    def __ne__(self, o : "Vector2"):
-        if not isinstance(o, Vector2):
-            raise TypeError("Can Only Compare With Type Vector2.")
-
-        return (self.x != o.x) or (self.y != o.y)
-
     def __neg__(self):
-        return Vector2(-self.x, -self.y)
+        return Vector2Int(-self.x, -self.y)
 
     def __str__(self):
         return f"({self.x}, {self.y})"
